0907e2e5032d60bb92e1f0cd973fb34cbec0b92b
Marcelo Junior <marcelojunior.tech@gmail.com>
2025-10-07 16:09:20 -0300

    15# Ajustes para Rodar na Azure com Docker

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,26 @@
-FROM node AS build
+FROM node:24-bookworm-slim AS build
+
 WORKDIR /usr/app
+
 COPY package.json ./
 COPY tsconfig.json ./
 COPY src ./src
+
 RUN npm install
 RUN npm run build
 
-FROM node
+FROM node:24-bookworm-slim
+
 WORKDIR /usr/app
+
 COPY package.json ./
 RUN npm install --production
+
 COPY --from=build /usr/app/dist ./dist
-COPY .env.example ./
-COPY .env ./
+COPY .env .env.example ./
+
 RUN npm install pm2 -g
+
 EXPOSE 5000
 CMD ["pm2-runtime", "dist/server.js"]
 
--- src/database/index.ts
+++ src/database/index.ts
@@ -4,20 +4,20 @@ import mongoose from 'mongoose'
 dotenv.config()
 
 async function main() {
+  var uri: string = '';
+
+  if (process.env.MONGO_URI == undefined) {
+    uri = `mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD
+      }@${!process.env.MONGO_DB_ADDRESS ? 'database' : process.env.MONGO_DB_ADDRESS
+      }:27017/icfj?authSource=admin`;
+  }
+  else {
+    uri = process.env.MONGO_URI;
+  }
+
   await mongoose.connect(
-    `mongodb://${process.env.MONGO_DB_USERNAME}:${
-      process.env.MONGO_DB_PASSWORD
-    }@${
-      !process.env.MONGO_DB_ADDRESS ? 'database' : process.env.MONGO_DB_ADDRESS
-    }:27017/icfj?authSource=admin`
-  )
-  console.log(
-    `Database connected to ${
-      !process.env.MONGO_DB_ADDRESS
-        ? 'database container'
-        : process.env.MONGO_DB_ADDRESS
-    }!`
-  )
+    uri,
+  );
 }
 
 main().catch((err) => console.log(err))
